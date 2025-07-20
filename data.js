53220aff00cd62570d915167c7705d6469a9a6e6
onyxpatel <onyxpatel@gmail.com>
2025-07-20 11:54:03 -0400

    data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const products = [
+  {
+    name: "Pokémon Scarlet & Violet 151 Elite Trainer Box",
+    upc: "820650853193",
+    msrp: "$49.99",
+    release_date: "2023-09-22",
+    retailers: {
+      Target: "https://www.target.com/p/...",
+      Walmart: "https://www.walmart.com/ip/...",
+      BestBuy: "https://www.bestbuy.com/site/..."
+    }
+  },
+  {
+    name: "Pokémon Temporal Forces Booster Box",
+    upc: "820650853469",
+    msrp: "$119.99",
+    release_date: "2024-03-22",
+    retailers: {
+      GameStop: "https://www.gamestop.com/...",
+      Amazon: "https://www.amazon.com/dp/..."
+    }
+  }
+];
