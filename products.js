7d8081967c8db1b7bd25a150d1972495c5c7d584
onyxpatel <onyxpatel@gmail.com>
2025-07-22 17:14:48 -0400

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 const products = [
-  { name: "Product 1", upc: "820650853193" },
-  { name: "Product 2", upc: "820650853537" },
-  { name: "Product 3", upc: "820650853674" },
-  { name: "Tide", upc: "037000875469" } // Tide
+  { name: "151 Elite Trainer Box", upc: "820650853193" },
+  { name: "Paldean Fates ETB", upc: "820650853537" },
+  { name: "Twilight Masquerade ETB", upc: "820650853674" },
+  { name: "Tide Detergent", upc: "037000875469" }
 ];
