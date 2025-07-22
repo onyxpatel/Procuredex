c9092ef16c60bede247f7930b8acdf73df335d80
onyxpatel <onyxpatel@gmail.com>
2025-07-22 16:12:06 -0400

    Update data.js

--- data.js
+++ data.js
@@ -5,9 +5,40 @@ const products = [
     msrp: "$49.99",
     release_date: "2023-09-22",
     retailers: {
-      Target: "https://www.target.com/p/...",
-      Walmart: "https://www.walmart.com/ip/...",
-      BestBuy: "https://www.bestbuy.com/site/..."
+      PokémonCenter: "https://www.pokemoncenter.com/product/290-85319/pokemon-tcg-scarlet-violet-151-elite-trainer-box",
+      Target: "https://www.target.com/p/pokemon-tcg-scarlet-violet-151-elite-trainer-box/-/A-89414703",
+      Amazon: "https://www.amazon.com/dp/B0CGLBLX5Z"
+    }
+  },
+  {
+    name: "Pokémon Paldean Fates Elite Trainer Box",
+    upc: "820650853537",
+    msrp: "$49.99",
+    release_date: "2024-01-26",
+    retailers: {
+      PokémonCenter: "https://www.pokemoncenter.com/product/290-85353/pokemon-tcg-paldean-fates-elite-trainer-box",
+      Target: "https://www.target.com/p/pokemon-tcg-paldean-fates-elite-trainer-box/-/A-89724441",
+      BestBuy: "https://www.bestbuy.com/site/pokemon-tcg-paldean-fates-elite-trainer-box/6579696.p"
+    }
+  },
+  {
+    name: "Pokémon Obsidian Flames Booster Box",
+    upc: "820650852141",
+    msrp: "$119.99",
+    release_date: "2023-08-11",
+    retailers: {
+      GameStop: "https://www.gamestop.com/toys-games/trading-cards/products/pokemon-tcg-scarlet-and-violet---obsidian-flames-booster-box/389160.html",
+      Amazon: "https://www.amazon.com/dp/B0C9SNN6QK"
+    }
+  },
+  {
+    name: "Pokémon Twilight Masquerade Elite Trainer Box",
+    upc: "820650853674",
+    msrp: "$49.99",
+    release_date: "2024-05-24",
+    retailers: {
+      PokémonCenter: "https://www.pokemoncenter.com/product/290-85367/pokemon-tcg-scarlet-violet-twilight-masquerade-elite-trainer-box",
+      Walmart: "https://www.walmart.com/ip/1779779984"
     }
   },
   {
@@ -16,8 +47,8 @@ const products = [
     msrp: "$119.99",
     release_date: "2024-03-22",
     retailers: {
-      GameStop: "https://www.gamestop.com/...",
-      Amazon: "https://www.amazon.com/dp/..."
+      Amazon: "https://www.amazon.com/dp/B0CQGQL9Q1",
+      GameStop: "https://www.gamestop.com/toys-games/trading-cards/products/pokemon-tcg-scarlet-and-violet---temporal-forces-booster-box/390347.html"
     }
   }
 ];
