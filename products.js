9ba510bf836b568920ff91c393a3d5048dbec6a1
onyxpatel <onyxpatel@gmail.com>
2025-07-22 17:06:02 -0400

    Create products.js

---
+++ products.js
@@ -0,0 +1,6 @@
+const products = [
+  { name: "Product 1", upc: "820650853193" },
+  { name: "Product 2", upc: "820650853537" },
+  { name: "Product 3", upc: "820650853674" },
+  { name: "Tide", upc: "037000875469" } // Tide
+];
